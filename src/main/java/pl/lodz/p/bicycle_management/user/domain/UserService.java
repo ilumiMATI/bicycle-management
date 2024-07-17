e7dc87f0bf7695d9296a34020530da69d1745726
ilumiMATI <mati98848@gmail.com>
2024-07-17 20:58:23 +0200

    Added basic User

---
+++ src/main/java/pl/lodz/p/bicycle_management/user/domain/UserService.java
@@ -0,0 +1,36 @@
+package pl.lodz.p.user_management.user.domain;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.lodz.p.bicycle_management.user.domain.User;
+import pl.lodz.p.bicycle_management.user.domain.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    final private UserRepository userRepository;
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findUserById(Integer id) {
+        return userRepository.findById(id);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(User user) {
+        return userRepository.update(user);
+    }
+
+    public void deleteUser(Integer id) {
+        userRepository.delete(id);
+    }
+}
